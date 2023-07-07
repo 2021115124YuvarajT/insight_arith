7bd79d60ddd453370e3660e4224264eef95558a7
yuvarajT-it <125898420+yuvarajT-it@users.noreply.github.com>
2023-07-07 21:33:33 +0530

    Update MulIntro.js

--- src/components/mul/MulIntro.js
+++ src/components/mul/MulIntro.js
@@ -7,7 +7,7 @@ export default function Intro() {
         <hr/>
             <p>Multiplication is probably the most important in arithmetic. It is the basis for many other operations. 
                 By improving the speed of multiplication, we can achieve spectacular performance in numerous other calculations.
-                We'll see two methods, which greatly improve the speed of multiplication. They are</p>
+                Also we'll see two methods, which greatly improve the speed of multiplication. They are</p>
             <ol style={ol}>
                 <li>Nikhilam</li>
                 <li>Urdhva Tiryagbyam</li>
